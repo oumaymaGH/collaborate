97a925f59868f32618786c22c802ee726918d129
OumaymaTaher <oumaymataher2018@gmail.com>
2023-07-06 11:46:05 +0300

    change in function_test is state is buy

--- ot-addons/market/models/market_products.py
+++ ot-addons/market/models/market_products.py
@@ -8,6 +8,7 @@ class MarketProducts(models.Model):
 
     message=fields.Text(string="Message")
     def function_test(self):
-        if self.state=='buy':
-            self.name="Amina"
-            print(self.name)
+        for rec in self :
+         if rec.state=='buy':
+            rec.name="Amina"
+            print(rec.name)
