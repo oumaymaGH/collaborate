4d4b5d353f3bca881581ce7354912dcf1e3209b5
OumaymaTaher <oumaymataher2018@gmail.com>
2023-07-06 11:38:15 +0300

    new module named : market is added

---
+++ ot-addons/market/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Market Module',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/appointment.xml',
+        'views/configuration.xml',
+        'views/beneficiary.xml',
+        'views/menu.xml',
+        'reports/report.xml'
+    ],
+    'application': True,
+}
---
+++ ot-addons/market/models/market_products.py
@@ -0,0 +1,12 @@
+from odoo import models,api,fields
+
+class MarketProducts(models.Model):
+    _name="market.products"
+
+    name=fields.Char(string="Product Name")
+    state = fields.Selection([('buy', 'Buy'), ('sell', 'Sell')],default='buy')
+
+    message=fields.Text(string="Message")
+    def function_test(self):
+        if self.state=='buy':
+            print("You will buy this product")
